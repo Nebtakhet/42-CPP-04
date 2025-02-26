43f57d58744a68016a275d189f8a00651cf46cf2
Cesar Sanchez <cesasanc@c1r6p2.hive.fi>
2025-02-26 16:36:06 +0200

    Updated main.cpp to test properly

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:29:42 by cesasanc          #+#    #+#             */
-/*   Updated: 2025/02/11 14:52:19 by cesasanc         ###   ########.fr       */
+/*   Updated: 2025/02/26 16:34:25 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,57 @@
 int	main()
 {
 	std::cout << "Animal tests:" << std::endl;
-	const Animal *i = new Dog();
-	const Animal *j = new Cat();
-	
-	std::cout << std::endl;
-	
-	std::cout << i->getType() << ": ";
-	i->makeSound();
+    const Animal *i = new Dog();
+    const Animal *j = new Cat();
 
+	std::cout << std::endl;
+    std::cout << i->getType() << ": ";
+	i->makeSound();
+	
 	std::cout << std::endl;
 	std::cout << j->getType() << ": ";
 	j->makeSound();
+	
+	std::cout << std::endl;
+    delete j;
+    delete i;
+
+	std::cout << std::endl;
+	std::cout << "Dog and Cat tests: \n" << std::endl;
+	Animal *animals[6];
+	for (int i = 0; i < 6; i++)
+	{
+		if (i % 2 == 0)
+			animals[i] = new Dog();
+		else
+			animals[i] = new Cat();
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		delete animals[i];
+	}
+	
+	std::cout << std::endl;
+	std::cout << "Dog tests: \n" << std::endl;
+	std::cout << "Original Dog:" << std::endl;
+	Dog originalDog;
+	originalDog.makeSound();
+	originalDog.getBrain()->setIdea(0, "I am a dog");
+	std::cout << originalDog.getBrain()->getIdea(0) << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Copy Dog:" << std::endl;
+	Dog copyDog(originalDog);
+	copyDog.makeSound();
+	std::cout << copyDog.getBrain()->getIdea(0) << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Assignation Dog:" << std::endl;
+	Dog assignationDog = originalDog;
+	assignationDog.makeSound();
+	std::cout << assignationDog.getBrain()->getIdea(0) << std::endl;
 
 	std::cout << std::endl;
 	
-	delete j;
-	delete i;
-
 	return (0);
 }
